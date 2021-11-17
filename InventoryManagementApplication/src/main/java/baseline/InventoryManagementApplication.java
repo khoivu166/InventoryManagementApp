44f22fe8bf25e7417df39c5b0b90c514fa4b1beb
Khoi Vu <khoivu166@gmail.com>
2021-11-17 01:11:00 -0500

    initial fxml

---
+++ InventoryManagementApplication/src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class InventoryManagementApplication extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene.fxml")));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
+
+        stage.setTitle("Inventory App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
